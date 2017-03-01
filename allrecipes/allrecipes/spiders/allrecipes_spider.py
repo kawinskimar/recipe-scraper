c5291ec15cf6180d6d1396e4a3dbfbfaf6e9ce40
Marissa Kawinski <marissakawinski@gmail.com>
2017-02-28 22:21:32 -0500

    Added return of list of relative links for JSON output, added non-working uniquify funciton
    Played with quite a bit of stuff, it's late but at least a small part of
    it works!

--- allrecipes/allrecipes/spiders/allrecipes_spider.py
+++ allrecipes/allrecipes/spiders/allrecipes_spider.py
@@ -1,17 +1,32 @@
 import scrapy
 
 class AllrecipesSpider(scrapy.Spider):
-    name = "allrecipes"
+    name = 'allrecipes'
+    allowed_domains = ['allrecipes.com']
+    download_delay = 1
 
     start_urls = [
             # American recipe categories
-            'http://allrecipes.com/recipes/201/meat-and-poultry/chicken/',
-            'http://allrecipes.com/recipes/148/breakfast-and-brunch/eggs/'
+            'http://allrecipes.com/recipes/659/meat-and-poultry/chicken/chicken-breasts/'#, # chicken breasts
+            #'http://allrecipes.com/recipes/148/breakfast-and-brunch/eggs/' # breakfasts with eggs
     ]
     
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'allrecipes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        #links = response.css('article.grid-col--fixed-tiles a::attr(href)').re(r'^\/recipe\/.*$')
+        #links = uniquify(links)
+
+        yield {
+            'relative_links': response.css('article.grid-col--fixed-tiles a::attr(href)').re(r'^\/recipe\/.*$')
+            # TODO: find only links that start with '/recipe/'
+            # TODO: remove duplicates from list of links
+        }
+
+    # not working the way I want it to
+    def uniquify(list):
+        # not order preserving, but quick - taken from https://www.peterbe.com/plog/uniqifiers-benchmark
+        set = Set(list)
+        return list(set)
+
+
+
 
